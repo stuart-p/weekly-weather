2e88ddd265ff5e10342a2f8868744b9e351b2bbf
stuart-p <stuart_palmer85@hotmail.com>
2020-02-15 17:51:27 +0000

    Updates

---
+++ precache-manifest.9974f750f28056606830f16212b3e49c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd2b67daa2cb513a94d6b6b2ec329093",
+    "url": "/weekly-weather/index.html"
+  },
+  {
+    "revision": "0420bdeb86f763fb7a68",
+    "url": "/weekly-weather/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ae76ab4eab3a61d97ea5",
+    "url": "/weekly-weather/static/js/2.349485bd.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/weekly-weather/static/js/2.349485bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0420bdeb86f763fb7a68",
+    "url": "/weekly-weather/static/js/main.eb0548c6.chunk.js"
+  },
+  {
+    "revision": "2d1079faf742db23cdfc",
+    "url": "/weekly-weather/static/js/runtime-main.fe882e9c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/weekly-weather/static/media/logo.5d5d9eef.svg"
+  }
+]);
